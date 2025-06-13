6bb7edc18b710e352d2fd28b4213dd18a4767d2f
matt.mcneil <matt.mcneil@innovapost.com>
2025-06-13 15:22:19 -0400

    Updated with  Maven pom.xml + converted - Deezer API data test being pulled correctly

--- src/src/main/java/com/mmcneil/musicstore/Main.java
+++ src/src/main/java/com/mmcneil/musicstore/Main.java
@@ -1,13 +1,18 @@
-package com.mmcneil.musicstore;
+package src.main.java.com.mmcneil.musicstore;
+
+import kong.unirest.Unirest;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
 
+    public static final Color BG_COLOR = Color.DARK_GRAY;
+
     public static void main(String[] args) {
         System.out.println("Welcome to the store. It's pretty bare around here for now.");
         createWindow();
+        System.out.println(Unirest.get("https://api.deezer.com/search?q=dinosaur").asJson().getBody().toPrettyString());
     }
 
     public static void createWindow() {
@@ -30,7 +35,7 @@ public class Main {
         panel.add(lblMain, BorderLayout.NORTH);
         panel.add(pnlSearch, BorderLayout.CENTER);
 
-        panel.setBackground(Color.DARK_GRAY);
+        panel.setBackground(BG_COLOR);
         lblMain.setForeground(Color.WHITE);
 
 
@@ -39,7 +44,7 @@ public class Main {
 
     private static JPanel createSearchPanel() {
         JPanel panel = new JPanel(new FlowLayout());
-        panel.setBackground(Color.DARK_GRAY);
+        panel.setBackground(BG_COLOR);
         JTextField txtSearch = new JTextField(20);
         JButton btnSearch = new JButton("Search");
 
